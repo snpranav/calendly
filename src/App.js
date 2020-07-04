822fe7345e08588c18775ce4181fc59a53623ce6
suhail <me@iamsuhail.com>
2020-07-04 22:19:19 +0530

    added favicon support

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
   dataURL = ('https://calendly.com/'+ user + window.location.pathname);
     render() {
       document.title = 'Find A Time With '+ name+' | '+ name+"'s Calendar Scheduling"
+  
       return (
      <div className="App">
       <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Favicon from 'react-favicon';
+
+const fav = process.env.REACT_APP_FAV;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  <Favicon url= {fav} />
+ , document.getElementById('root')
 );
 
 
