e0b1d72de3ce1fd3b21811b728dc6c5c180f466c
suhail <me@iamsuhail.com>
2020-07-04 22:28:11 +0530

    reversed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const user = process.env.REACT_APP_USER;
 const name = process.env.REACT_APP_NAME;
 
+
 export default class App extends Component {
 
   dataURL = ('https://calendly.com/'+ user + window.location.pathname);
@@ -10,7 +11,8 @@ export default class App extends Component {
       document.title = 'Find A Time With '+ name+' | '+ name+"'s Calendar Scheduling"
   
       return (
-     <div className="App">
+     
+      <div className="App">
       <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
       </div>
    
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Favicon from 'react-favicon';
 
-const fav = process.env.REACT_APP_FAV;
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  <Favicon url= {fav} />
+  </React.StrictMode>
  , document.getElementById('root')
 );
 
