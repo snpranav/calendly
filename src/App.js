acc97c857213ced84670e0f3e38f4fdc112ffa87
suhail <me@iamsuhail.com>
2020-07-04 19:18:37 +0530

    updated name gen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 const user = process.env.REACT_APP_USER;
+const name = process.env.REACT_APP_NAME;
 
 export default class App extends Component {
 
   dataURL = ('https://calendly.com/'+ user + window.location.pathname);
     render() {
-
+      document.title = 'Find A Time With' + {name}
       return (
      <div className="App">
       <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
