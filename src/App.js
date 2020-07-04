5496b67c2ca9250d93041fb787a3dc5cb36d7ffd
suhail <me@iamsuhail.com>
2020-07-04 16:40:42 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+var user = "chinyasuhail"
+
+
+
+
+
+export default class App extends Component {
+
+
+  dataURL = ('https://calendly.com/'+ user + window.location.pathname);
+  render() {
+    return (
+      <div className="App">
+      <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
+      </div>
+    );
+  }
+}
