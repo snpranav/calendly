96baf976e8288bfc017dade8ae67a2133f90733d
suhail <me@iamsuhail.com>
2020-07-04 16:49:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-         <small>You are running this application in <b>{process.env.user}</b></small>
       <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
       </div>
     );
