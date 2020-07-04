7931c44d56b8cb75586cabf4d5af433e204ecc48
Pranav Shikarpur <contact@snpranav.com>
2020-07-04 19:07:56 -0400

    Moved scripts to component file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ const name = process.env.REACT_APP_NAME;
 
 
 export default class App extends Component {
-  dataURL = ('https://calendly.com/'+ user + window.location.pathname);
+  componentDidMount() {
+    document.title = `Find A Time With ${name} | ${name}'s Calendar Scheduling`;
+    const calendlyScript = document.createElement("script")
+    calendlyScript.src = "https://assets.calendly.com/assets/external/widget.js";
+    calendlyScript.type = "text/javascript";
+    calendlyScript.async = true;
+    document.body.appendChild(calendlyScript)
+  }
+
+  dataURL = `https://calendly.com/${user}${window.location.pathname}`;
     render() {
-      document.title = `Find A Time With ${name} | ${name}'s Calendar Scheduling`;
       return (
      <div className="App">
        <div className="calendly-inline-widget col-12" data-url={this.dataURL} style={{ position:"absolute", top:0, height:"100%", width:"100%" }}></div>
